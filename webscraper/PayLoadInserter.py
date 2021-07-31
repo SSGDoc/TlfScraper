09b166a94a73f37af062a0857650e855424262d6
ssgdoc <1068772@ucn.dk>
2021-07-31 14:43:52 +0200

    'Comments'

--- webscraper/PayLoadInserter.py
+++ webscraper/PayLoadInserter.py
@@ -1,15 +1,18 @@
 import mysql.connector
 from mysql.connector import Error
 
+# Matcher Navne i Databasen med Navne fundet med Scraperen
 def importPhoneNumbers(name, phoneNum):
     try:
+        # Opret forbindelse til databasen
         connection = mysql.connector.connect(user='root', password='digital', host='127.0.0.1', port='3306', auth_plugin='mysql_native_password', database = "datawarehouse")
         cursor = connection.cursor(prepared=True)
+        # SQL query
         cmd = """UPDATE virksomhed SET tlf = %s WHERE UPPER(navn) = %s"""
         data_tuple= (phoneNum, name)
         cursor.execute(cmd, data_tuple)
         connection.commit()
-        
+        # Throw on query fail
     except mysql.connector.Error as error:print("parameterized query failed {}".format(error))
     
     finally:
--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -16,9 +16,10 @@ uClient.close()
 mainPage_soup = soup(mainPage_html, "html.parser")
 print("Finder links...", end = '')
 
-#Find alle links
+#Find alle links på siden
 links = []
 linkContainers = mainPage_soup.findAll("a", {"class":"summarizedCategory"})
+# Dictionary med NAVN,TLF
 allCompanies = {}
 
 for link in linkContainers:
@@ -29,16 +30,16 @@ for link in linkContainers:
     token = urllib.parse.urlunsplit(token)
     links.append(token)
 print("Done!")
-#Find alle phone containers på alle links
 
-print("Finder navne og tlf numre...", end = '')
+#Find alle tags med telefon numre på alle siderne på alle links
+print("Finder navne og tlf numre lol...", end = '')
 for link in links:
     placeHolderURL = link
     newUClient = uReq(placeHolderURL)
     tokenPage_html = newUClient.read()
     tokenPage_soup = soup(tokenPage_html, "html.parser")
     allDetailContainers = tokenPage_soup.find_all("span", {"class": "details"})
-
+    # Frasortér unødig HTML fra de nestede telefon nummrer tags
     for detail in allDetailContainers:
         phoneNum = detail.find("span", {"class":"phone"})
         phoneNum = phoneNum.text.strip()[5:]
@@ -49,22 +50,24 @@ for link in links:
         navn = navn.replace(',','')
         navn = navn.replace("'", "")
         navn = navn.replace('"','')
-
+        # Tilføj nyt sæt i allCompanies med NAVN,TLF
         allCompanies[navn] = phoneNum
 
 newUClient.close()
 print("Done!")
 
-print("Opretter csv fil...", end = '')
+print("Opretter csv fil lmao...", end = '')
 fileName = "telefonNumre.csv"
 f = open(fileName, "w")
 
 f.write("navn,tlf\n")
 print("Done!")
-print("Indsætter tlf numre i databasen...", end = '')
+print("Indsætter tlf numre i CSV filen lol...", end = '')
+# Iterér gennem allCompanies Dictionary'en og tilføj ny linje i CSV filen med X og Y værdien
+# såfremt længden af navnet er større end 0. (Undgår blanke felter)
 for x, y in allCompanies.items():
     if(len(y) > 0):
         f.write(x +","+ y +"\n")
-        inserter.importPhoneNumbers(x,y)
+        #inserter.importPhoneNumbers(x,y)
 f.close()
 print("Done!")
